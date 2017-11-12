822992833da15f935b11403933cb735e48f632f8
Miguel Yax <mig_dj@hotmail.com>
2017-11-12 11:51:35 -0600

    Minimal server definition

---
+++ server.js
@@ -0,0 +1,11 @@
+/*!
+ * @author Miguel Yax <mig_dj@hotmail.com>
+ * date 11/11/2017
+ * Skills API RESTful
+ */
+
+const express = require('express'),
+    app = express(),
+    port = process.env.PORT || 3000;
+
+app.listen(port);
