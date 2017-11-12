c01a6b54ad9fe5f35877c29a5372f103270391e5
Miguel Yax <mig_dj@hotmail.com>
2017-11-12 11:52:50 -0600

    add visual studio code configurations

---
+++ controllers/person.js
@@ -0,0 +1,42 @@
+/*!
+ * @author Miguel Yax <mig_dj@hotmail.com>
+ * date 11/11/2017
+ * Person API RESTful - Controller
+ */
+
+const mongoose = require('mongoose'),
+    person = mongoose.model('Persons');
+
+let controller = {
+
+    list: function (req, res) {
+        var personList = [];
+
+        return personList;
+    },
+
+    add: function (req, res) {
+        var person = {};
+
+        return person;
+    },
+
+    get: function (req, res) {
+        var id = '';
+
+        return id;
+    },
+
+    update: function (req, res) {
+        var person = null;
+
+        return person;
+    },
+    remove: function (req, res) {
+        var removed = false;
+
+        return removed;
+    },
+};
+
+exports = controller; 
